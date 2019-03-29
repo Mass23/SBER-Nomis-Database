6178579356c5057a5e136264921076d9d309ca36
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-29 11:23:41 +0100

    Update update_db.py

--- update_db.py
+++ update_db.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import glob
+import os
+
+list_sub = glob.glob(*)
+
+dirs = [i for i in list_sub if os.path.isdir(i) == True]
+files = [i for i in list_sub if os.path.isfile(i) == True]
