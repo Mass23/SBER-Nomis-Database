54de3cc7e0962923f7bbd37334763df343712abc
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-04-01 15:31:00 +0200

    Update update_db.py

--- update_db.py
+++ update_db.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 import pandas as pd
-import glob
 import os
+import glob
 import time
+import statistics.stdev as stdev
 
-################ Functions ################
-def add_row(df, row):
-    df.loc[-1] = row
-    df.index = df.index + 1
-    return df.sort_index()
-
+################ Logfile functions ################
 def LogUpdate(log):
     log.write('-----------------------------')
     log.write('\n')
@@ -29,45 +25,99 @@ def LogUpdate(log):
     log.write('-----------------------------')
     log.write('\n')
 
+################ Check IDs functions ################
+def StrInt(string):
+    try:
+        int(string)
+        return(True)
+    except ValueError:
+        return(False)
+
+def CheckGlacierID(glacier_id):
+    if glacier_id.startswith('GL') and StrInt(glacier_id.replace('GL','')) and not glacier_id.replace('GL','').startswith('0'):
+        return(True)
+    else:
+        return(False)
+
+def CheckLocation(location):
+    if location in ['UP','DN']:
+        return(True)
+    else:
+        return(False)
+
+def CheckPatchID(patch_id):
+    if patch_id in [1,2,3]:
+        return(True)
+    else:
+        return(False)
+
+################ Table management functions ################
+def AddRow(table, row):
+    table.loc[-1] = row
+    table.index = table.index + 1
+    return table.sort_index()
+
+#def AppendData(table, data_to_append):
+
+################ Data query functions ################
 def GetAvailableGlaciers(file):
     with open(file) as f:
+        glaciers_data = pd.read_csv(f, delimiter = '\t', header = 0, names = ['glacier', 'location', 'patch'])
+        print(glaciers_data)
         glaciers_list = []
 
-        for glacier in f.readlines():
-            glaciers_list.append(glacier.rstrip('\n'))
+        for index, row in enumerate(glaciers_data.itertuples()):
+            print(row)
+            glaciers_list.append(row.glacier)
 
-        return(glaciers_list)
+        return(sorted(list(set(glaciers_list))))
+
+def LoadTSV(file):
+    with open(file, 'r') as f:
+        data = pd.read_csv(f, sep = '\t', header = 1)
 
 ################ Main #####################
 def main():
     with open('database.log', 'w') as log:
-        # 0. Write some basic information on the log file, create the table
+
+        # 0. Write some basic information on the log file
         LogUpdate(log)
 
-        glaciers_list = GetAvailableGlaciers('glacier_list.tsv')
+        # 1. Check that directories are present for all the metrics
+        directories = [i for i in glob.glob(*) if os.isdir(i)]
+        files = [i for i in glob.glob(*) if os.isfile(i)]
 
+        field_parameters = 'field_data.tsv'
+        if field_parameters not in files:
+            log.write('Field parameters file not found! exiting...')
+            return(-1)
+
+        lab_parameters = ['BA','Chl-a','DOC','EEA','EPS','BP','DOM','EGM','mDOM','Resp']
+        for i in lab_parameters:
+            if i not in directories:
+                log.write('Following parameter directory not found: ' + i)
+
+        dna_parameters = ['DNA']
+
+        # 2. Create the dataframe
+        glaciers_list = GetAvailableGlaciers(field_parameters)
         data_table = pd.DataFrame(columns = ['glacier', 'location', 'patch'])
 
         for glacier in glaciers_list:
-            add_row(data_table, [glacier,'UP',1])
-            add_row(data_table, [glacier,'UP',2])
-            add_row(data_table, [glacier,'UP',3])
-            add_row(data_table, [glacier,'DN',1])
-            add_row(data_table, [glacier,'DN',2])
-            add_row(data_table, [glacier,'DN',3])
+            AddRow(data_table, [glacier,'UP',1])
+            AddRow(data_table, [glacier,'UP',2])
+            AddRow(data_table, [glacier,'UP',3])
+            AddRow(data_table, [glacier,'DN',1])
+            AddRow(data_table, [glacier,'DN',2])
+            AddRow(data_table, [glacier,'DN',3])
 
-        log.write('  - ')
         log.write(str(len(glaciers_list)))
-        log.write(' glaciers')
+        log.write('glaciers:')
+        for i in glaciers_list:
+            log.write(' - ' + i)
         log.write('\n\n')
 
-        # 1. List all files and subdirectories
-        list_sub = glob.glob('*')
-
-        dirs = [i for i in list_sub if os.path.isdir(i) == True]
-        files = [i for i in list_sub if os.path.isfile(i) == True]
-
-        samples_metrics = ['BA', 'BP', 'Chl-a', 'DNA', 'DOC', 'DOM', 'EEA', 'EGM', 'EPS', 'Ions', 'mDOM', 'minerals', 'Nutrients', 'Resp']
+        # 3. Add columns with values
 
 if __name__== "__main__":
     main()
